d8061277bdea17d22d2706fdef9170369061f689
SpaceGameDev568 <spacegamedev568@gmail.com>
2023-04-11 08:06:28 -0400

    Fixed some errors

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,11 @@
       /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
       function openNav() {
         document.getElementById("websidebar").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
+        // Disabled as it caused an error but does not appear to be needed:  document.getElementById("main").style.marginLeft = "250px";
       }
 
       /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
       function closeNav() {
         document.getElementById("websidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
+        // Disabled as it caused an error but does not appear to be needed:  document.getElementById("main").style.marginLeft = "0";
       }
