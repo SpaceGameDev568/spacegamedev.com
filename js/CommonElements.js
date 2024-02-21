9220df386b7532f5906aa29a4d4cdc46490964f6
Jesse Hodgson <space@spacegamedev.com>
2024-02-20 20:40:50 -0500

    New Header/Footer

---
+++ js/CommonElements.js
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/a/31837264/19932427
+
+$(function () {
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'elements/' + $(this).data('include') + '.html'
+    $(this).load(file)
+  })
+})
