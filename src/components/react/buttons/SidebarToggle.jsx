1922d6de1ce154e484d121a4f7e4292315e541ea
Jesse Hodgson <space@spacegamedev.com>
2024-10-28 20:30:16 -0400

    10/28/2024 Start of the week changes

--- src/components/react/buttons/SidebarToggle.jsx
+++ src/components/react/buttons/SidebarToggle.jsx
@@ -6,17 +6,22 @@ function SidebarToggle() {
 
   const [state, setState] = useState("Collapsed");
 
+  const buttonElement = document.getElementById('material-icon');
+
+
   function toggleSidebar() {
     if(state === "Collapsed") {
       setState("Expanded");
       const sidebar = document.getElementById('sidebar');
       sidebar.style.left = '0px';
       sidebar.style.transition = '800ms cubic-bezier(0.34, 1.56, 0.64, 1)';
+      buttonElement.innerHTML = 'close';
     } else {
       setState("Collapsed");
       const sidebar = document.getElementById('sidebar');
       sidebar.style.left = sidebar.style.width;
       sidebar.style.transition = '800ms cubic-bezier(0.36, 0, 0.66, -0.56)';
+      buttonElement.innerHTML = 'menu';
     }
 
   }
@@ -27,7 +32,7 @@ function SidebarToggle() {
 
           <button className={"sidebar-button-toggle"} onClick={toggleSidebar}>
 
-            <span className="material-symbols-rounded icon-large"> menu </span>
+            <span className="material-symbols-rounded icon-large" id={"material-icon"}> menu </span>
 
           </button>
 
