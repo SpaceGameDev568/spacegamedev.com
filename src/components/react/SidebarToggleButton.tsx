bde6c273d149c675e9cb5671002f77996b9c1bb7
Jesse Hodgson <space@spacegamedev.com>
2025-02-06 20:10:20 -0500

    Upgrade to Astro 5.2.5 and TailwindCSS 4

--- src/components/react/SidebarToggleButton.tsx
+++ src/components/react/SidebarToggleButton.tsx
@@ -38,9 +38,9 @@ function SidebarToggleButton() {
 
   return (
       <>
-          <button className={"material-symbols-rounded aspect-square h-[calc(100%-0.5rem)] mt-1 ml-1 text-4xl hover:bg-blue-500 duration-300 rounded-lg text-white bg-transparent select-none hover:ring-4 border-[1px] border-opacity-10 border-white"} id={"material-icon"} title={"Toggle Sidebar"} onClick={toggleSidebar}>
+          <button className={"material-symbols-rounded aspect-square h-[calc(100%-0.5rem)] mt-1 ml-1 hover:bg-blue-500 duration-300 rounded-lg text-white bg-transparent select-none hover:ring-4 hover:ring-blue-500/50 cursor-pointer border-[1px] border-white/10"} id={"material-icon"} title={"Toggle Sidebar"} onClick={toggleSidebar}>
 
-            menu
+            <p className={"text-4xl"}>menu</p>
 
           </button>
       </>
